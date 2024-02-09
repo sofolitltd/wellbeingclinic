3d04b0c458903328c3501555c9236db0458d7ac5
sofolitltd <sofolitltd@gmail.com>
2024-02-10 02:30:10 +0600

    fix des, image, color

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 const Color kGreenColor = Color(0xffD3EFD8);
 const Color kPurpleColor = Color(0xffddc3f6);
 const Color kYellowColor = Color(0xffFFF5C0);
-const Color kBlueColor = Color(0xffCDE2F3);
+const Color kBlueColor = Color(0x7e8cb0ff);
 const Color kPinkColor = Color(0xffFFEEE4);
 const Color kLightBlueColor = Color(0xffF1F4F9);
 const Color kMediumBlueColor = Color(0xffbddef1);
